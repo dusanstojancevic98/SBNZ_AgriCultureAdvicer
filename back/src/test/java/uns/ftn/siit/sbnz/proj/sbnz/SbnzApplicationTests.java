520a5630ed084b6301b7089ed2e7845273fc0a24
nikolappc <neekolapapic@gmail.com>
2021-05-05 14:11:53 +0200

    Spring init

---
+++ back/src/test/java/uns/ftn/siit/sbnz/proj/sbnz/SbnzApplicationTests.java
@@ -0,0 +1,13 @@
+package uns.ftn.siit.sbnz.proj.sbnz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbnzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
