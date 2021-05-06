3cd4b4d6908d6c06a4c7930221f6053be4c770d1
skyaltair <skyaltair98@gmail.com>
2021-05-06 14:52:56 +0200

    Ispravljena izmena globalne promenljive

--- back/src/main/java/uns/ftn/siit/sbnz/proj/sbnz/controller/OdabirUsevaController.java
+++ back/src/main/java/uns/ftn/siit/sbnz/proj/sbnz/controller/OdabirUsevaController.java
@@ -1,14 +1,12 @@
 package uns.ftn.siit.sbnz.proj.sbnz.controller;
 
 
-import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import uns.ftn.siit.sbnz.proj.sbnz.model.*;
 import uns.ftn.siit.sbnz.proj.sbnz.service.OdabirUsevaService;
