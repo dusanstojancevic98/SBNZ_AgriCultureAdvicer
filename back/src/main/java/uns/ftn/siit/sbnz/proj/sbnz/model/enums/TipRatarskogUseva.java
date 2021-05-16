9f2a29cdfa6ac21618c57b6d93e71862bb95813d
nikolappc <neekolapapic@gmail.com>
2021-05-16 23:57:31 +0200

    Dodati neki enumi

---
+++ back/src/main/java/uns/ftn/siit/sbnz/proj/sbnz/model/enums/TipRatarskogUseva.java
@@ -0,0 +1,10 @@
+package uns.ftn.siit.sbnz.proj.sbnz.model.enums;
+
+public enum TipRatarskogUseva {
+    ZITARICE,
+    TRAVE,
+    DETELINE,
+    ULJANI_USEVI, //ULJANA REPICA I SUNCOKRET
+
+
+}
---
+++ back/src/main/java/uns/ftn/siit/sbnz/proj/sbnz/model/enums/TipVocnogUseva.java
@@ -0,0 +1,5 @@
+package uns.ftn.siit.sbnz.proj.sbnz.model.enums;
+
+public enum TipVocnogUseva {
+    CITRUSI, JABUCASTI, KOSTUNICAVI, BOBICASTI, ORASASTI
+}
